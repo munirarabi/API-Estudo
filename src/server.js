14c25b075d7e8896af2fda24875e4cd010da4162
Munir Arabi <munirarabi04@gmail.com>
2023-04-06 01:17:36 -0300

    updates

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const app = require("./app");
 
-
 require("dotenv").config();
-// const PORT = process.env.PORT || 3306;
-const PORT = 3306;
+// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || PORT;
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
